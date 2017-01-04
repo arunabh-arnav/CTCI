68626d8a6b388774f8222ff05d364062ba95bfc7
aarnav <arunabh.arnav@gmail.com>
2017-01-04 15:03:29 -0500

    The hello world commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!\nThis is my first formal usage of GitHub for my own programs. Yayy!" << endl;
+    return 0;
+}
